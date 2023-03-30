ce4049e979aa5146a1a93d6ae7f42cad2337cfa6
Augusto <autologon@CSED.LAB>
2023-03-30 14:07:11 -0300

    Atualizando dia3

--- Dia 03/setup/urls.py
+++ Dia 03/setup/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from videogame_store.views import JogoViewSet, LojaViewSet
 from rest_framework import routers
 
-router = routers.DefaultRout()
-router.register('jogos', JogoViewSet, basename = 'Jogos')
-router.register('lojas', LojaViewSet, basename = 'Loja')
+router = routers.DefaultRouter()
+router.register('jogos', JogoViewSet, basename='Jogos')
+router.register('lojas', LojaViewSet, basename='Loja')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- Dia 03/videogame_store/admin.py
+++ Dia 03/videogame_store/admin.py
@@ -3,10 +3,10 @@ from .models import Jogo, Loja
 
 class JogosAdmin(admin.ModelAdmin):
     list_display = ('nome', 'preco')
-    
+
 admin.site.register(Jogo, JogosAdmin)
 
 class LojaAdmin(admin.ModelAdmin):
     list_display = ('nome', 'endereco', 'telefone')
-    
+
 admin.site.register(Loja, LojaAdmin)
